c7eeebe50cc0e7ca91cbe17c3a2fee7aa7b5f7af
micahmisterlal <michamisterlal96@gmail.com>
2025-03-21 14:28:51 +0300

    my first commit

---
+++ login.js
@@ -0,0 +1,8 @@
+document.getElementById("form1").addEventListener("submit", function(event){
+    event.preventDefault();
+    const formData = new FormData(this);
+    
+    for (const[name, value] of formData.entries()){
+        console.log(`${name}: ${value}`);
+    }
+});
